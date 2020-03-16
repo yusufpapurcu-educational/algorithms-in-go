84dc9ed3f1016ec5d0ec505b53622d4385bb156b
yusufpapurcu <yusufturhanp@gmail.com>
2020-03-16 13:25:24 +0300

    Linear Serach Added

--- Seraching/LinearSerach/linear_test.go
+++ Seraching/LinearSerach/linear_test.go
@@ -1,4 +1,4 @@
-package binaryserach
+package LinearSerach
 
 import (
 	"fmt"
@@ -7,11 +7,11 @@ import (
 	"testing"
 )
 
-func TestBinarySearch(t *testing.T) {
+func TestLinearSerach(t *testing.T) {
 	a := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55} // Test Dizisi
 	x := a[rand.Int()%len(a)]                       // Istenen eleman
 
-	i := Search(len(a), func(i int) bool { return a[i] >= x }) // Arama Algoritması
+	i := LinearSerach(a, x) // Arama Algoritması
 	if i < len(a) && a[i] == x {
 		fmt.Printf("%d Sayısı dizinin %d. Indexinde bulundu. Dizi :  %v\n", x, i, a)
 	} else {
@@ -19,14 +19,13 @@ func TestBinarySearch(t *testing.T) {
 	}
 }
 
-func BenchmarkBinarySearch(b *testing.B) {
+func BenchmarkLinearSerach(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		a := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55} // Test Dizisi
 		x := a[rand.Int()%len(a)]                       // Istenen eleman
-
-		i := Search(len(a), func(i int) bool { return a[i] >= x }) // Arama Algoritması
+		i := LinearSerach(a, x)                         // Arama Algoritması
 		if i < len(a) && a[i] == x {
-			fmt.Printf("%d Sayısı dizinin %d. Indexinde bulundu. Dizi :  %v\n", x, i, a)
+			//	fmt.Printf("%d Sayısı dizinin %d. Indexinde bulundu. Dizi :  %v\n", x, i, a)
 		} else {
 			log.Fatalf("%d Sayısı, %v Dizisi içinde bulunamadı\n", x, a)
 		}
---
+++ Sorting/SelectionSort/selection.go
@@ -0,0 +1,5 @@
+package SelectionSort
+
+func SelectionSort(array []int) {
+
+}
