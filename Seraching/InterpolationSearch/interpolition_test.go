41350375505197c0b6d3e92580ec1fb3250b2366
Yusuf Papurcu <yusufturhanp@gmail.com>
2021-08-24 21:38:57 +0300

    (cleaning): Interpolition Search tests cleaned

--- Seraching/InterpolationSearch/interpolition_test.go
+++ Seraching/InterpolationSearch/interpolition_test.go
@@ -1,34 +1,38 @@
 package interpolition
 
 import (
-	"fmt"
-	"log"
-	"math/rand"
 	"testing"
 )
 
-func TestJumpSearch(t *testing.T) {
-	a := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610} // Test Dizisi
-	x := a[rand.Int()%len(a)]                                               // Istenen eleman
+func TestInterpolitionSearch(t *testing.T) {
+	t.Run("sampleDataset", func(t *testing.T) {
+		for wantedNumber, dataSample := range sampleDataset {
+			result := Search(dataSample, wantedNumber)
+			if result < len(dataSample) && dataSample[result] == wantedNumber {
+				t.Logf("Data Sample: %v\nNumber %d found in %d index.\n", dataSample, wantedNumber, result)
+			} else {
+				t.Fatalf("Data Sample: %v\nNumber %d can't found in Data Sample.\n", wantedNumber, dataSample)
+			}
+		}
+	})
+}
 
-	i := Search(a, x) // Arama Algoritması
-	if i < len(a) && a[i] == x {
-		//		fmt.Printf("%d Sayısı dizinin %d. Indexinde bulundu. Dizi :  %v\n", x, i, a)
-	} else {
-		t.Fatalf("%d Sayısı, %v Dizisi içinde bulunamadı\n", x, a)
+func BenchmarkInterpolitionSearch(b *testing.B) {
+	dataSample := []int{1, 3, 6, 10, 15, 21, 28, 36, 45, 55}
+	wantedNumber := 36
+
+	for i := 0; i < b.N; i++ {
+		result := Search(dataSample, wantedNumber)
+		if !(result < len(dataSample) && dataSample[result] == wantedNumber) {
+			b.Fatalf("Data Sample: %v\nNumber %d can't found in Data Sample.\n", wantedNumber, dataSample)
+		}
 	}
 }
 
-func BenchmarkJumpSearch(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		a := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610} // Test Dizisi
-		x := a[rand.Int()%len(a)]                                               // Istenen eleman
-
-		i := Search(a, x) // Arama Algoritması
-		if i < len(a) && a[i] == x {
-			fmt.Printf("%d Sayısı dizinin %d. Indexinde bulundu. Dizi :  %v\n", x, i, a)
-		} else {
-			log.Fatalf("%d Sayısı, %v Dizisi içinde bulunamadı\n", x, a)
-		}
-	}
+var sampleDataset = map[int][]int{
+	45:  {1, 3, 6, 10, 15, 21, 28, 36, 45, 55},
+	129: {1, 43, 52, 78, 110, 129, 160, 172, 188, 201},
+	2:   {0, 2, 14, 33, 67, 79, 91, 112, 142, 149},
+	902: {24, 31, 47, 69, 89, 103, 210, 578, 901, 902},
+	12:  {12, 37, 62, 102, 151, 213, 285, 362, 455, 551},
 }
